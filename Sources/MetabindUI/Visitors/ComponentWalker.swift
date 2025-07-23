7a6f6b415805fa14e3708a1843d828d231be4214
Ollie Wagner <ollie@yapstudios.com>
2025-07-23 14:56:41 -0600

    Simplify Walker

--- Sources/MetabindUI/Visitors/ComponentWalker.swift
+++ Sources/MetabindUI/Visitors/ComponentWalker.swift
@@ -4,18 +4,8 @@ public protocol ComponentWalker: ComponentVisitor where Result == Void {
 
 public extension ComponentWalker {
     mutating func descendInto(_ component: Component) {
-        let mirror = Mirror(reflecting: component)
-        for child in mirror.children {
-            if let component = child.value as? Component {
-                visit(component)
-            } else if let components = child.value as? [Component] {
-                for component in components {
-                    visit(component)
-                }
-            } else if let optionalComponent = child.value as? Optional<Component>,
-                      let component = optionalComponent {
-                visit(component)
-            }
+        for child in component.componentChildren {
+            visit(child)
         }
     }
 }
