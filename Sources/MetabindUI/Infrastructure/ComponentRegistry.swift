07ce570e943ff83b72142ece2be8bded3500b328
Ollie Wagner <ollie@yapstudios.com>
2025-08-15 09:47:36 -0600

    Include ComponentContext in ComponentRepresentableContext

--- Sources/MetabindUI/Infrastructure/ComponentRegistry.swift
+++ Sources/MetabindUI/Infrastructure/ComponentRegistry.swift
@@ -1,19 +1,18 @@
 import SwiftUI
 
 struct ComponentRegistry {
-    var components: [String: ([String: Any], [Component]) -> AnyView] = [:]
+    var components: [String: ([String: Any], [Component], ComponentContext?) -> AnyView] = [:]
     
-    mutating func register<C: View>(_ name: String, @ViewBuilder build: @escaping (_ props: [String: Any], _ children: [Component]) -> C) {
-        components[name] = {
-            AnyView(build($0, $1))
+    mutating func register<C: View>(_ name: String, @ViewBuilder build: @escaping (_ props: [String: Any], _ children: [Component], _ componentContext: ComponentContext?) -> C) {
+        components[name] = { props, children, context in
+            AnyView(build(props, children, context))
         }
     }
     
     func makeComponent(_ name: String, props: [String: Any], children: [Component], componentContext: ComponentContext) -> AnyView? {
         guard let builder = components[name] else { return nil }
         return AnyView(
-            builder(props, children)
-                .environmentObject(componentContext)
+            builder(props, children, componentContext)
         )
     }
 }
@@ -25,9 +24,12 @@ extension EnvironmentValues {
 extension View {
     public func component<R: ComponentRepresentable>(_ component: R) -> some View {
         transformEnvironment(\.componentRegistry) { registry in
-            registry.register(component.name) { props, children in
-                component.makeView(context: .init(children: children, props: props))
-                    .environmentObject(ComponentContext())
+            registry.register(component.name) { props, children, componentContext in
+                component.makeView(context: .init(
+                    children: children,
+                    props: props,
+                    componentContext: componentContext
+                ))
             }
         }
     }
