475db504375fbeffefd6be53bb058bb7af00e24d
David Fumberger <dave@fumberger.com>
2025-12-11 11:22:13 +1100

    Update ContentAction

--- Sources/BindJS/Core/ContentAction.swift
+++ Sources/BindJS/Core/ContentAction.swift
@@ -1,9 +1,31 @@
-public struct ContentAction: Codable, Hashable, Sendable {
-    public let name: String
-    public let props: [String: String]?
+import SwiftUI
 
-    public init(name: String, props: [String: String] = [:]) {
+public struct ContentAction: Decodable, Sendable {
+    public let name: String
+    public let props: [String: Any]
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case props
+    }
+
+    public init(name: String, props: [String: Any] = [:]) {
         self.name = name
         self.props = props
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        // Decode "name"
+        self.name = try container.decode(String.self, forKey: .name)
+
+        // Decode props using AnyDecodable to Any
+        let rawProps = try container
+            .decodeIfPresent([String: AnyDecodable].self, forKey: .props)
+            ?? [:]
+
+        // Convert AnyDecodable to Any
+        self.props = rawProps.mapValues(\.value)
     }
 }
